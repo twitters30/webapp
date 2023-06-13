7b8ed4edff88bb59d240fcfdc57f23a713231bdd
Jesse Makanju <davidmakanju@gmail.com>
2023-06-13 00:12:45 +0000

    Resolved

--- app.java
+++ app.java
@@ -2,4 +2,7 @@
 # Please do not hard code
 # make user of variables and dynamic variables
 # author: David Makanju
+<<<<<<< HEAD
 #This is a fe application
+=======
+>>>>>>> a7ebfb5 (first commmit$)
